0c0b99212f16941460b71fdef0c16c9aa02f0b96
glg37 <bielgutjahr1@gmail.com>
2025-02-20 16:38:26 -0300

    scripts

---
+++ aula Pc/Assets/scripts/CharacterStatus.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class CharacterStatus : MonoBehaviour
+{
+    [SerializeField] float lifeMax;
+    [SerializeField] float speed;
+    float life;
+
+    public float LifeMax { get => lifeMax;}
+    public float Speed { get => speed;}
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        life = LifeMax;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    protected abstract void Teste();//Obriga o filho a implementar 
+    protected virtual void Teste2()//Permite que o filho sobrescreva
+    {
+
+    }
+}
